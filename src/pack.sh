b83e17c15044f18422a65000cc65b626a1750f1f
Mpho Kekana <mphokekana@.com>
2023-06-14 07:48:54 -0700

    Update

---
+++ src/pack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env Bash
+python -m pip install --upgrade pip
+pip install Flask
+pip install sqlalchemy
+pip install flasksqlalchemy
+pip install uuid
+pip install gunicorn
+pip install -r requirements.txt
